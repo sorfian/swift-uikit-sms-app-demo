98f6fcea5a8183923f4763addf9d2837b2c0856e
Sorfian <sorfian.tkj@gmail.com>
2023-03-26 13:16:29 +0800

    Initial Commit

---
+++ SMS Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMS Demo App
+//
+//  Created by Sorfian on 26/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
